af83d0d9371ab5e19e4f5b6c4c00eb81e89756d8
Eirik Vigeland <eirik.vigeland@bekk.no>
2021-10-21 22:12:15 +0200

    Improvements

--- src/Card.mjs
+++ src/Card.mjs
@@ -1,7 +1,7 @@
 import React from 'https://esm.sh/react@17'
 
 const Card = (props) => {
-  const { title, text, time, onDelete } = props;
+  const { title, text, time, img, onDelete } = props;
 
   return (
     React.createElement(
@@ -14,7 +14,7 @@ const Card = (props) => {
           'img',
           {
             className: 'mt-2 rounded-full w-8 h-8 sm:w-10 sm:h-10',
-            src: 'https://images.unsplash.com/photo-1604426633861-11b2faead63c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&h=200&q=80',
+            src: img,
           },
         ),
       ),
